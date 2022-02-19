051aee9dda3741cb7ac684f9c17794c8bb05c492
SeifElmotaz <seifelmotaz@outlook.com>
2022-02-19 02:54:07 +0200

    spaceadmin

--- spaceadminn/linux/flutter/generated_plugins.cmake
+++ spaceadminn/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  window_manager
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
--- spaceadminn/windows/flutter/generated_plugin_registrant.cc
+++ spaceadminn/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  WindowManagerPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
